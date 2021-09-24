01e537e52e75daaba47b7ec21ab8936c50bd9fb9
Shreyashmahajan <bevantara947@gmail.com>
2021-09-24 23:46:23 +0530

    angle of triangle html js

---
+++ is-Triangle/isTriangle.js
@@ -0,0 +1,22 @@
+var angleOne = document.querySelector("#angle1");
+var angleTwo = document.querySelector("#angle2");
+var angleThree = document.querySelector("#angle3");
+var buttonCheck = document.querySelector("#button-check");
+var displayOutput = document.querySelector("#output-box");
+
+function checkTriangle(){
+    var a = Number(angleOne.value);
+    var b = Number(angleTwo.value);
+    var c = Number(angleThree.value);
+    if( (a&&b&&c) != 0) {
+     if((a+b+c) === 180) {
+      displayOutput.innerText = "It is a Triangle";
+     } else {
+         displayOutput.innerText = "It is Not a Triangle";
+     }}else {
+         displayOutput.innerText = "please Input Correct Values";
+     }
+}
+
+buttonCheck.addEventListener("click", checkTriangle);
+
